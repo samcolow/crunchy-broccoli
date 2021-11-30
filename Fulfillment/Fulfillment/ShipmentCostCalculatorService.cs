3c30ff5ea8533409f8c97ebc12e93396ef17a977
Sam Low <sam.low@outlook.com>
2021-11-30 22:29:40 +1300

    Add TODO notes for step 3, 4, 5

--- Fulfillment/Fulfillment/ShipmentCostCalculatorService.cs
+++ Fulfillment/Fulfillment/ShipmentCostCalculatorService.cs
@@ -16,6 +16,10 @@ namespace Fulfillment
         {
             var items = ProcessInputItems(input).ToList();
 
+            // TODO: Step 3 - a separate process to validate the weight needs to be introduced and cost adjusted if overweight
+
+            // TODO: Step 5 - a separate process to adjust the cost due to multiple parcel discount
+
             return new ShipmentCostResult(items);
         }
 
@@ -51,6 +55,7 @@ namespace Fulfillment
                 ItemSize.Medium => MediumSizeCost,
                 ItemSize.Large => LargeSizeCost,
                 ItemSize.XLarge => XLargeSizeCost,
+                // TODO: Step 4 - implementation for the Heavy item size
                 _ => throw new NotSupportedException(),
             };
         }
